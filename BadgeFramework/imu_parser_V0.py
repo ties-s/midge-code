a23be0deac9f55c3db941d3b5d74c10253b6873b
Bent Engbers <bentengbers@gmail.com>
2021-12-08 13:14:40 +0100

    improve parser, add gitignore

--- BadgeFramework/imu_parser_V0.py
+++ BadgeFramework/imu_parser_V0.py
@@ -4,16 +4,91 @@ from datetime import datetime as dt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from inspect import getsourcefile
+import os
+from os.path import abspath
+from typing import Final
+from datetime import datetime
+import shutil
+import sys
+# Constants
+OUTPUT_DIRECTORY: Final = "output"
+OUTPUT_RAW_DIRECTORY: Final = "output_raw"
+
+ACCELERATION_POSTFIX: Final = '_accel'
+GYROSCOPE_POSTFIX: Final = "_gyr"
+MAGNETOMETER_POSTFIX: Final = "_mag"
+ROTATION_POSTFIX: Final = "_rotation"
+
+
+def get_BadgeFrame_Directory():
+    return os.path.dirname(abspath(getsourcefile(lambda: 0)))
+
+
+def create_path_if_not_exists(path, force):
+    if os.path.exists(path) and os.listdir(path) != 0 and force:
+        try:
+            shutil.rmtree(path)
+        except OSError as e:
+            print("Error: %s - %s." % (e.filename, e.strerror))
+    if not os.path.exists(path):
+        print(f"creating  non existing directory: {path}")
+        os.makedirs(path)
+        return
+    if os.listdir(path) != 0 and not force:
+        shortpath = os.sep.join(os.path.normpath(path).split(os.sep)[-2:])
+        sys.exit(
+            f"Stopping: directory {shortpath} is not empty, retry with -f if you want to override the data")
+
+
+def parsedTimeStamp(timestamp):
+    return datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d_%H:%M:%S')
+
+
+def fullTimeName(timestamp):
+    return f"{parsedTimeStamp(timestamp)}_{timestamp}"
+
+
+class SDFiles(object):
+
+    def __init__(self, path_file_sd, force):
+        self.force = force
+        timestamp = int(os.path.basename(path_file_sd))
+        self.full_file_name = fullTimeName(timestamp)
+        self.raw_directory = os.path.join(get_BadgeFrame_Directory(
+        ), OUTPUT_RAW_DIRECTORY, self.full_file_name)
+        self.path_accel_sd = path_file_sd + ACCELERATION_POSTFIX
+        self.path_gyro_sd = path_file_sd + GYROSCOPE_POSTFIX
+        self.path_mag_sd = path_file_sd+MAGNETOMETER_POSTFIX
+        self.path_rotation_sd = path_file_sd+ROTATION_POSTFIX
+
+    def outputfile(self, postfix):
+        return os.path.join(self.raw_directory, f"{self.full_file_name}{postfix}")
+
+    def moveFiles(self):
+        print(f"moving raw files to {self.raw_directory}")
+        create_path_if_not_exists(self.raw_directory, force=self.force)
+        shutil.copy2(self.path_accel_sd, self.outputfile(ACCELERATION_POSTFIX))
+        shutil.copy2(self.path_gyro_sd, self.outputfile(GYROSCOPE_POSTFIX))
+        shutil.copy2(self.path_mag_sd, self.outputfile(MAGNETOMETER_POSTFIX))
+        shutil.copy2(self.path_rotation_sd, self.outputfile(ROTATION_POSTFIX))
+
+    def parserFromCopiedFiles(self):
+        return IMUParser(os.path.join(self.raw_directory, self.full_file_name), force=self.force)
 
 
 class IMUParser(object):
 
-    def __init__(self, filename):
-        self.filename = filename
-        self.path_accel = filename+'_accel'
-        self.path_gyro = filename+'_gyr'
-        self.path_mag = filename+'_mag'
-        self.path_rotation = filename+'_rotation'
+    def __init__(self, filename, force):
+        self.force = force
+        self.file_base_path = filename
+        self.path_accel = filename+ACCELERATION_POSTFIX
+        self.path_gyro = filename+GYROSCOPE_POSTFIX
+        self.path_mag = filename+MAGNETOMETER_POSTFIX
+        self.path_rotation = filename+ROTATION_POSTFIX
+        self.file_base_name = os.path.basename(self.file_base_path)
+        self.output_directory_path = os.path.join(get_BadgeFrame_Directory(),
+                                                  OUTPUT_DIRECTORY, self.file_base_name)
 
     def parse_generic(self, sensorname):
         data = []
@@ -75,56 +150,58 @@ class IMUParser(object):
         df['b'] = rotation_xyz[:, 1]
         df['c'] = rotation_xyz[:, 2]
         df['d'] = rotation_xyz[:, 2]
-        print(df)
         self.rot_df = df
 
-    def plot_and_save(self, a, g, m):
-        if a:
-            fname = self.filename + '_accel.png'
+    def plot_and_save(self, acc, gyr, mag):
+        if acc:
+            acc_file_name = os.path.join(
+                self.output_directory_path, f"{self.file_base_name}_{ACCELERATION_POSTFIX}.png")
             ax = self.accel_df.plot(x='time')
             fig = ax.get_figure()
-            fig.savefig(fname)
-        if g:
-            fname = self.filename + '_gyro.png'
+            fig.savefig(acc_file_name)
+        if gyr:
+            gyr_file_name = os.path.join(
+                self.output_directory_path, f"{self.file_base_name}_{GYROSCOPE_POSTFIX}.png")
             ax = self.gyro_df.plot(x='time')
             fig = ax.get_figure()
-            fig.savefig(fname)
-        if m:
-            fname = self.filename + '_mag.png'
+            fig.savefig(gyr_file_name)
+        if mag:
+            mag_file_name = os.path.join(
+                self.output_directory_path, f"{self.file_base_name}_{MAGNETOMETER_POSTFIX}.png")
             ax = self.mag_df.plot(x='time')
             fig = ax.get_figure()
-            fig.savefig(fname)
+            fig.savefig(mag_file_name)
 
-    def save_dataframes(self, a, g, m, r):
-        if a:
-            self.accel_df.to_pickle(self.path_accel + '.pkl')
-            self.accel_df.to_csv(self.path_accel + '.csv')
-        if g:
-            self.gyro_df.to_pickle(self.path_gyro + '.pkl')
-            self.gyro_df.to_csv(self.path_gyro + '.csv')
-        if m:
-            self.mag_df.to_pickle(self.path_mag + '.pkl')
-            self.mag_df.to_csv(self.path_mag + '.csv')
-        if r:
-            print(f"BROOOOOO ${self.path_rotation}")
+    def save_dataframes(self, acc, gyr, mag, rot):
+        create_path_if_not_exists(self.output_directory_path, force=self.force)
+        print(f"saving parsed data to: {self.output_directory_path}")
 
-            self.rot_df.to_pickle(self.path_rotation + '.pkl')
-            self.rot_df.to_csv(self.path_rotation + '.csv')
-
-
-def str2bool(v):
-    if isinstance(v, bool):
-        return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
+        if acc:
+            acc_file_path = os.path.join(
+                self.output_directory_path, f"{self.file_base_name}{ACCELERATION_POSTFIX}")
+            self.accel_df.to_pickle(acc_file_path + '.pkl')
+            self.accel_df.to_csv(acc_file_path + '.csv')
+        if gyr:
+            gyr_file_path = os.path.join(
+                self.output_directory_path, f"{self.file_base_name}{GYROSCOPE_POSTFIX}")
+            self.gyro_df.to_pickle(gyr_file_path + '.pkl')
+            self.gyro_df.to_csv(gyr_file_path + '.csv')
+        if mag:
+            mag_file_path = os.path.join(
+                self.output_directory_path, f"{self.file_base_name}{MAGNETOMETER_POSTFIX}")
+            self.mag_df.to_pickle(mag_file_path + '.pkl')
+            self.mag_df.to_csv(mag_file_path + '.csv')
+        if rot:
+            rot_file_path = os.path.join(
+                self.output_directory_path, f"{self.file_base_name}{ROTATION_POSTFIX}")
+            self.rot_df.to_pickle(rot_file_path + '.pkl')
+            self.rot_df.to_csv(rot_file_path + '.csv')
 
 
-def main(fn, acc, mag, gyr, rot, plot):
-    parser = IMUParser(fn)
+def main(file, acc, mag, gyr, rot, plot, force):
+    sdfiles = SDFiles(file, force=force)
+    sdfiles.moveFiles()
+    parser = sdfiles.parserFromCopiedFiles()
     if acc:
         parser.parse_accel()
     if mag:
@@ -142,18 +219,20 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Parser for the IMU data obtained from Minge Midges\
     (Acceleration, Gyroscope, Magnetometer, Rotation)')
-    parser.add_argument('--fn', required=True,
+    parser.add_argument('file',
                         help='Please enter the path to the file')
-    parser.add_argument('--acc', default=True, type=str2bool,
-                        help='Check to parse and save acceleration data')
-    parser.add_argument('--mag', default=True, type=str2bool,
-                        help='Check to parse and save magnetometer data')
-    parser.add_argument('--gyr', default=True, type=str2bool,
-                        help='Check to parse and save gyroscope data')
-    parser.add_argument('--rot', default=True, type=str2bool,
-                        help='Check to parse and save rotation data')
-    parser.add_argument('--plot', default=True, type=str2bool,
-                        help='Check to plot the parsed data')
+    parser.add_argument('--no-acc', action="store_false",
+                        help='add this flag to not parse accelerometer data (default=FALSE)')
+    parser.add_argument('--no-mag', action="store_false",
+                        help='add this flag to not parse magnetometer data (default=FALSE)')
+    parser.add_argument('--no-gyr', action="store_false",
+                        help='add this flag to not parse gyroscope data (default=FALSE) ')
+    parser.add_argument('--no-rot', action="store_false",
+                        help='add this flag to not parse rotation data from the DMP (default=FALSE)')
+    parser.add_argument('--no-plot', action="store_false",
+                        help='add this flag to not plot the data from the accelerometer, gyroscope and magnetometer (default=FALSE)')
+    parser.add_argument('-f', action="store_true",
+                        help=f"add this flag to override the subdirectories in '{OUTPUT_DIRECTORY}' and '{OUTPUT_RAW_DIRECTORY}' (default=FALSE)")
     args = parser.parse_args()
-    main(fn=args.fn, acc=args.acc, mag=args.mag,
-         gyr=args.gyr, rot=args.rot, plot=args.plot)
+    main(file=args.file, acc=args.no_acc, mag=args.no_mag,
+         gyr=args.no_gyr, rot=args.no_rot, plot=args.no_plot, force=args.f)
