513cc045b40ff1169ed6dd34602661f8900cfed9
Bent Engbers <bentengbers@gmail.com>
2021-12-14 22:51:58 +0000

    add rotation file

--- BadgeFramework/rotation/rotate.py
+++ BadgeFramework/rotation/rotate.py
@@ -1,13 +1,35 @@
-from typing import Final, TypedDict
+import sys
+from typing import Final
 import pigpio
-
+import json
+from pydantic import BaseModel, ValidationError
+from enum import Enum
 DIR: Final = 20     # Direction GPIO Pin
 STEP: Final = 12    # Step GPIO Pin
+INSTRUCTION_FILE: Final = "./rotation_instructions.json"
 
 
-class RampInstruction(TypedDict):
+class DirectionEnum(str, Enum):
+    left = 'left'  # Counter clockwise
+    right = 'right'  # Clockwise
+
+
+def pulseDirection(direction: DirectionEnum) -> pigpio.pulse:
+    if(direction == DirectionEnum.right):
+        # Turn on
+        return pigpio.pulse(1 << DIR, 0, 0)
+    # Turn off
+    return pigpio.pulse(0, 1 << DIR, 0)
+
+
+class Instruction(BaseModel):
     freq: int
     steps: int
+    dir: DirectionEnum
+
+
+class InstructionList(BaseModel):
+    __root__: list[Instruction]
 
 
 # Connect to pigpiod daemon
@@ -18,7 +40,7 @@ pi.set_mode(DIR, pigpio.OUTPUT)
 pi.set_mode(STEP, pigpio.OUTPUT)
 
 
-MODE:Final = (14, 15, 18)   # Microstep Resolution GPIO Pins
+MODE: Final = (14, 15, 18)   # Microstep Resolution GPIO Pins
 RESOLUTION: Final = {'Full': (0, 0, 0),
                      'Half': (1, 0, 0),
                      '1/4': (0, 1, 0),
@@ -29,7 +51,7 @@ for i in range(3):
     pi.write(MODE[i], RESOLUTION['Full'][i])
 
 
-def generate_ramp(instruction_list: list[RampInstruction]):
+def executeInstructions(instruction_list: list[Instruction]):
     """Generate ramp wave forms.
     ramp:  List of [Frequency, Steps]
     """
@@ -39,10 +61,11 @@ def generate_ramp(instruction_list: list[RampInstruction]):
 
     # Generate a wave per ramp level
     for i in range(length):
-        frequency = instruction_list[i]["freq"]
+        frequency = instruction_list[i].freq
         micros = int(500000 / frequency)
         wf: list[pigpio.pulse] = []
         wf.append(pigpio.pulse(1 << STEP, 0, micros))  # pulse on
+        wf.append(pulseDirection(instruction_list[i].dir))
         wf.append(pigpio.pulse(0, 1 << STEP, micros))  # pulse off
         pi.wave_add_generic(wf)
         wid[i] = pi.wave_create()
@@ -50,7 +73,7 @@ def generate_ramp(instruction_list: list[RampInstruction]):
     # Generate a chain of waves
     chain = []
     for i in range(length):
-        steps = instruction_list[i]["steps"]
+        steps = instruction_list[i].steps
         x = steps & 255
         y = steps >> 8
         chain += [255, 0, wid[i], 255, 1, x, y]
@@ -58,4 +81,21 @@ def generate_ramp(instruction_list: list[RampInstruction]):
     pi.wave_chain(chain)  # Transmit chain.
 
 
-generate_ramp([{'freq': 1000, 'steps': 200}])
+def verifyFile(path: str) -> InstructionList:
+    f = open(path)
+    instructions = json.load(f)
+    if not isinstance(instructions, list):
+        sys.exit("Stopping: the root element of a file is not an array")
+    try:
+        return InstructionList(__root__=instructions)
+    except ValidationError as e:
+        sys.exit(f"Stopping: {e}")
+
+
+def main():
+    instructionList = verifyFile(INSTRUCTION_FILE).__root__
+    executeInstructions(instructionList)
+
+
+if __name__ == "__main__":
+    main()
